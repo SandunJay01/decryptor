a0329a6dc9a6aaebffef4ef4e5f195e32ac8ac75
SandunJay01 <SandunJ@cbctechsol.com>
2023-09-07 09:54:32 +0530

    Initial commmit

---
+++ src/main/java/com/cbc_tech/decryption/DecryptorApplication.java
@@ -0,0 +1,13 @@
+package com.cbc_tech.decryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecryptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecryptorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cbc_tech/decryptor/DecryptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbc_tech.decryptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecryptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
